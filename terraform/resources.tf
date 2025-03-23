41324080aae8d0ad39f74096fdbf35e63e45748c
jmpanadero <jmpanadero@uma.es>
2025-03-23 22:40:25 +0100

    Correccion de errores de sintaxis 5

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -31,7 +31,7 @@ resource "azurerm_linux_virtual_machine" "lvm" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = var.registry_name
+  name                = "jmppcp2acr"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
   sku                 = "Basic"
